994f2ca2bbf7c757b7d3221d6ef4016c3fc0bd8a
pocomane <none@mail.com>
2023-07-11 11:01:43 +0200

    change

--- util/pdfize.lua
+++ util/pdfize.lua
@@ -2,39 +2,7 @@
 
 package.path = package.path .. ';util/?.lua'
 
-local function render(x)
-
-  local t, e = io.open('build/tmp.md', 'w')
-  if e then error(e) end
-  t:write(x,'\n')
-  t:close()
-  local t, err = io.popen('markdown -f footnotes -f fencedcode build/tmp.md')
-  if e then error(e) end
-  x = t:read('a')
-  t:close()
-
-  x = [[
-  <!doctype html>
-  <html>
-  <head>
-      <title>Example</title>
-      <meta charset="utf-8" />
-      <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <style type="text/css">
-        @page {
-          size: A4;
-          margin-left: 75pt;
-          margin-right: 75pt;
-          margin-bottom: 75pt;
-          margin-top: 60pt;
-        }
-        body {
-          column-count: 2;
-          font: 10pt "Sans";
-          line-height: 14pt;
-          text-align: justify;
-        }
+local common_css = [[
         /*
         .PageBreak {
           page-break-after: always;
@@ -97,17 +65,125 @@ local function render(x)
           margin-bottom: 2pt;
           margin-top: 2pt;
         }
+]]
+
+local standard_css = [[
+        @page {
+          size: A4;
+          margin-left: 75pt;
+          margin-right: 75pt;
+          margin-bottom: 75pt;
+          margin-top: 60pt;
+        }
+        body {
+          column-count: 2;
+          font: 10pt "Sans";
+          line-height: 14pt;
+          text-align: justify;
+        }
+]]
+
+local standard_html = [[
+  <!doctype html>
+  <html>
+  <head>
+      <title>Example</title>
+      <meta charset="utf-8" />
+      <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <style type="text/css">
+]] .. common_css .. standard_css .. [[
       </style>    
   </head>
   <body>
   <div>
-  ]] .. x .. [[
+]]
+
+
+local compact_css = [[
+        @page {
+          size: A4 landscape;
+          margin-left: 75pt;
+          margin-right: 75pt;
+          margin-bottom: 75pt;
+          margin-top: 60pt;
+        }
+        body {
+          column-count: 3;
+          font: 10pt "Sans";
+          line-height: 14pt;
+          text-align: justify;
+        }
+]]
+
+local compact_html = [[
+  <!doctype html>
+  <html>
+  <head>
+      <title>Example</title>
+      <meta charset="utf-8" />
+      <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <style type="text/css">
+]] .. common_css .. compact_css .. [[
+      </style>    
+  </head>
+  <body>
+  <div>
+]]
+
+local end_html = [[
   </div>
   </body>
   </html>
-  ]]
+]]
 
-  return x
+local function render(x, pre, pos)
+
+  local t, e = io.open('build/tmp.md', 'w')
+  if e then error(e) end
+  t:write(x,'\n')
+  t:close()
+  local t, err = io.popen('markdown -f footnotes -f fencedcode build/tmp.md')
+  if e then error(e) end
+  x = t:read('a')
+  t:close()
+  return  pre .. x .. pos
+
+end
+
+local function make_single_pdf(src, dst, mode)
+  if '' ~= dst then
+    dst = dst:gsub('%..*$','')
+    print('generating '..dst..'.pdf')
+
+    local f, e = io.open(src, 'r')
+    if e then error(e) end
+    local content = f:read('a')
+    f:close()
+
+    local x
+    if not  mode or 'default' == mode then
+      x = render(content, standard_html, end_html)
+    elseif mode == 'compact' then
+      x = render(content, compact_html, end_html)
+    else
+      error('unknown output mode "' .. mode .. '"')
+    end
+
+    x = x:gsub('<pre><code>(.-)</code></pre>',function(content)
+      return '<div class="example"><p>'..(content:gsub('(\n\r?\n)','</p>%1<p>'))..'</p></div>'
+    end)
+
+    local f, e = io.open('build/'..dst..'.html', 'wb')
+    if e then error(e) end
+
+    f:write(x)
+    f:close()
+
+    local ok, st, err = os.execute('weasyprint build/'..dst..'.html build/'..dst..'.pdf')
+    if 'exit' ~= st then error(err) end
+  end
 end
 
 local function make_pdf()
@@ -118,33 +194,12 @@ bita.md
 bita-strong.md
 ]]
 
+  local count = 0
   for src in fileliststr:gmatch('[^\n]*') do
+    count = count + 1
 
     local dst = src:match('[^/\\]*$')
-    if '' ~= dst then
-      dst = dst:gsub('%..*$','')
-      print('generating '..dst..'.pdf')
-
-      local f, e = io.open(src, 'r')
-      if e then error(e) end
-      local content = f:read('a')
-      f:close()
-
-      local x=render(content)
-
-      x = x:gsub('<pre><code>(.-)</code></pre>',function(content)
-        return '<div class="example"><p>'..(content:gsub('(\n\r?\n)','</p>%1<p>'))..'</p></div>'
-      end)
-
-      local f, e = io.open('build/'..dst..'.html', 'wb')
-      if e then error(e) end
-
-      f:write(x)
-      f:close()
-
-      local ok, st, err = os.execute('weasyprint build/'..dst..'.html build/'..dst..'.pdf')
-      if 'exit' ~= st then error(err) end
-    end
+    make_single_pdf(src, dst, count == 1 and 'default' or 'compact')
   end
 end
 

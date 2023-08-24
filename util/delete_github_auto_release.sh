5cb2498feee3e22adda5d1391cd21cd18375cbdb
pocomane <none@mail.com>
2023-08-24 11:28:20 +0200

    change

--- util/delete_github_auto_release.sh
+++ util/delete_github_auto_release.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "deleteing tags"
-gh release list | tail -n +2 | awk '{if ($2 ~ /^release\.[a-zA-Z0-9]*$/) {print $2}}' | while read -r line; do git push origin --delete  "$line"; done
+gh release list | tail -n +2 | awk '{if ($1 ~ /^release\.[a-zA-Z0-9]*$/) {print $2}}' | while read -r line; do git push origin --delete  "$line"; done
 
 echo "deleteing release"
 gh release list | tail -n +2 | awk '{if ($1 ~ /^release\.[a-zA-Z0-9]*$/) {print $1}}' | while read -r line; do gh release delete -y "$line"; done
